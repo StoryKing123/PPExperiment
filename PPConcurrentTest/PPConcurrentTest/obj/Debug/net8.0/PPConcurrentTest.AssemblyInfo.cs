87a5d5306cc9d97aa872b72fc9b603fab888425f
禤志涛 <xuanzhitao@192.168.5.75>
2025-04-06 21:36:51 +0800

    feat new connection pool

--- PPConcurrentTest/PPConcurrentTest/obj/Debug/net8.0/PPConcurrentTest.AssemblyInfo.cs
+++ PPConcurrentTest/PPConcurrentTest/obj/Debug/net8.0/PPConcurrentTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PPConcurrentTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6973c6f2e66e7bfa5de3d63c2372f7bd55887a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d079a19685ec311506d3af58132b581298cd06e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PPConcurrentTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PPConcurrentTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
